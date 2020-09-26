48daeadb4e593a710a4961e8ce7e68d646fa1f6b
avacodehub <avacodehub@gmail.com>
2020-09-26 22:17:32 +0300

    email query for user (restriction)

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -5,7 +5,7 @@ import {
   Arg,
   Ctx,
   ObjectType,
-  Query,
+  Query, FieldResolver, Root
 } from "type-graphql";
 import { MyContext } from "../types";
 import { User } from "../entities/User";
@@ -33,8 +33,18 @@ class UserResponse {
   user?: User;
 }
 
-@Resolver()
+@Resolver(User)
 export class UserResolver {
+  @FieldResolver(() => String)
+  email(@Root() user: User, @Ctx() {req}: MyContext) {
+    //user can 'get' only his own email 
+    if(req.session.userId === user.id){
+      return user.email
+    } else {
+      return ""
+    }
+  }
+  
   @Mutation(() => UserResponse)
   async changePassword(
     @Arg("token") token: string,
